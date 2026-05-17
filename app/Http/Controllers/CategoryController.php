370c2cecb85a0fde07c09fc0f276a9efe48de31d
Algis Asmaradhana JW <gisssss.jw@gmail.com>
2026-05-17 19:36:04 +0800

    buat dan isi file categorycontroller.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -1,1 +1,23 @@
+<?php
 
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class CategoryController extends Controller {
+    public function index() {
+        $categories = Category::all();
+        return view('categories.index', compact('categories'));
+    }
+
+    public function store(Request $request) {
+        Category::create($request->all());
+        return back();
+    }
+
+    public function destroy(Category $category) {
+        $category->delete();
+        return back();
+    }
+}
